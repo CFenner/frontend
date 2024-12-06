7bdb01e0dd3fcac7d5c31bcb719d62acf470c900
Bram Kragten <mail@bramkragten.nl>
2024-12-06 20:21:15 +0100

    fix translations

--- src/panels/config/integrations/ha-config-integration-page.ts
+++ src/panels/config/integrations/ha-config-integration-page.ts
@@ -936,7 +936,7 @@ class HaConfigIntegrationPage extends SubscribeMixin(LitElement) {
           ${item.supports_subentries
             ? html`<ha-md-menu-item
                 @click=${this._addSubEntry}
-                .entryId=${item.entry_id}
+                .entry=${item}
                 graphic="icon"
               >
                 <ha-svg-icon slot="start" .path=${mdiPlus}></ha-svg-icon>
@@ -1589,8 +1589,8 @@ class HaConfigIntegrationPage extends SubscribeMixin(LitElement) {
   }
 
   private async _addSubEntry(ev) {
-    showSubConfigFlowDialog(this, {
-      startFlowHandler: ev.target.entryId,
+    showSubConfigFlowDialog(this, ev.target.entry, {
+      startFlowHandler: ev.target.entry.entry_id,
     });
   }
 
